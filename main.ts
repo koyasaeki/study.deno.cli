2efa2896b601d81801a2e68e7f042aafd5dd3f68
KoyaSaeki <koyasaeki@gmail.com>
2022-12-18 16:56:41 +0900

    update: 受け取ったコマンドの引数を表示する

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.5/command/mod.ts";
 
-await new Command()
+const { args } = await new Command()
   .name("cliffy")
   .version("0.1.0")
   .description("Command line framework for Deno")
@@ -18,3 +18,5 @@ await new Command()
   .arguments("<input:string> [output:string]")
   .action((_options, ..._args) => console.log("Bar command called."))
   .parse(Deno.args);
+
+console.log(args);
